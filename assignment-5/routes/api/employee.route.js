6409b13968cf89d2f354dd481d08ff657a134228
Jonathan <jdoolittle126@gmail.com>
2022-02-14 15:44:21 -0500

    Week 5 Homework

--- assignment-5/routes/api/employee.route.js
+++ assignment-5/routes/api/employee.route.js
@@ -4,6 +4,14 @@ const dbo = require('../db/connection');
 const {ObjectId} = require("mongodb");
 
 
+const routeEmployees = router.route('employees');
+
+routeEmployees.get(((req, res) => {
+
+
+
+    res.json()
+}));
 
 // Expose the router to the application
 module.exports = router;
